49d3830cc248c4baa319824a7f6aa9b69f8fb5f9
Kathryn Stapleton <kathryn.stapleton777@gmail.com>
2018-07-03 14:55:55 -0400

    made a ChatHeader module

---
+++ src/ChatHeader.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ChatHeader = () => {
+    return (
+    <div className="chatHeader">
+        <div class="roomInfo">
+        <h2>#general</h2>
+        <p>Announcements and general chat</p>
+        </div>
+        </div>
+    )
+
+}
+
+export default ChatHeader
